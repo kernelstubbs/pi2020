0dade0e001ae021da1e3541a75cf53a0e6984d89
Kristopher Stubbe <stubbe@Potemkin.beowulf.cc>
2020-03-24 15:25:44 -0400

    Intermediate done

--- Solutions/Beginner.py
+++ Solutions/Beginner.py
@@ -1,10 +1,10 @@
 import unicornhat as hat
 import time
 
-hat.set_layout(hat.AUTO)
 hat.rotation(90)
 hat.brightness(0.25)        
-col,row = hat.get_shape() # Universal for hat and hatHD 8x8 or 16x16 grids
+width,height = hat.get_shape() # Universal for hat and hatHD 8x8 or 16x16 grids
+
 
 red     = (255, 0, 0)
 green   = (0, 255, 0)
@@ -33,7 +33,7 @@ hat.clear()
 ### Do the same, but using a FOR loop to use only one set_pixel() line
 ##
 
-for i in range(col):
+for i in range(width):
     x = i
     y = i
     hat.set_pixel(x,y, *blue)
@@ -47,9 +47,9 @@ hat.clear()
 ### Switch the slash from the previous challenges to go from 0-7 to 7-0
 ##
 
-for i in range(col):
+for i in range(width):
     x = i
-    y = 7 - i
+    y = height - 1 - i 
     hat.set_pixel(x, y, *green)
 hat.show()
 time.sleep(5)
@@ -57,11 +57,11 @@ hat.clear()
 
 ##
 ### Challenge #4
-### Light one column using a FOR loop
+### Light one widthumn using a FOR loop
 ##
 
-y = 0 # which column to light 0-7
-for x in range(col):
+y = 0 # which widthumn to light 0-7
+for x in range(width):
     hat.set_pixel(x, y, *red)
 
 hat.show()
@@ -70,11 +70,11 @@ hat.clear()
 
 ##
 ### Challenge #5
-### Light one row using a FOR loop
+### Light one height using a FOR loop
 ##
 
-x = 0 # which column to light 0-7
-for y in range(row):
+x = 0 # which widthumn to light 0-7
+for y in range(height):
     hat.set_pixel(x, y, *red)
 
 hat.show()
@@ -85,9 +85,9 @@ hat.clear()
 ### BONUS - Make an X
 ##
 
-for i in range(col):
+for i in range(width):
     x1 = i
-    x2 = 7 - i
+    x2 = width - 1 - i
     y = i
     hat.set_pixel(x1, y, *red)
     hat.set_pixel(x2, y, *blue)
--- Solutions/Intermediate.py
+++ Solutions/Intermediate.py
@@ -4,30 +4,111 @@ import time
 hat.set_layout(hat.AUTO)
 hat.rotation(90)
 hat.brightness(0.25)        
-col,row = hat.get_shape() # Universal for hat and hatHD 8x8 or 16x16 grids
+width,height = hat.get_shape() # Universal for hat and hatHD 8x8 or 16x16 grids
 
 red     = (255, 0, 0)
 green   = (0, 255, 0)
 blue    = (0, 0, 255)
+fucshia = (255, 0, 170)
+off     = (0, 0, 0)
+
+def display(duration): # Instead of repesting these three lines throughout, we can call them as display()
+        hat.show()
+        time.sleep(duration)
+        hat.clear()
 
 ##
 ### Challenge #1
 ### Make an oscillating dot, like Knight Rider or a Cylon
 ##
 
-# Cylon
-
-[int]y = row / 2 # place dot in teh middle-ish
+y = height / 2 # place dot in the middle-ish
 for n in range(5):
-    for i in range(col):
+    for i in range(width):
         x = i 
         hat.set_pixel(x, y, *red)
-        hat.show()
-        time.sleep(0.2)
-        hat.clear()
-    for i in range(col):
-        x = 7 - i
+        display(0.2)
+    for i in range(width):
+        x = width - 1 - i
         hat.set_pixel(x, y, *red)
-        hat.show()
-        time.sleep(0.2)
-        hat.clear()
+        display(0.2)
+# Why 'width - 1 - i' - Because width is equal to "8" but the pixel value can only be 0 to 7
+# we offset to align with Pythonic ranges. 0-1-2-3 vs 1-2-3-4
+
+## Bonus: Don't let the dot linger on the first or last position
+## Hint: range(n,n)
+
+y = height / 2 # place dot in the middle-ish
+for n in range(5):
+    for i in range(width):
+        x = i 
+        hat.set_pixel(x, y, *red)
+        display(0.2)
+    for i in range(1,width - 1):
+        x = width - 2 - i
+        hat.set_pixel(x, y, *red)
+        display(0.2)
+
+##
+### Challenge #2
+### Make an oscillating column in a non-primary colour
+##
+
+for n in range(5):
+    for x in range(width):
+        for y in range(height):
+            hat.set_pixel(x, y, *fucshia)
+            display(0.2)
+    for i in range(1, width - 1):
+        for y in range(height):
+            x = width - 1 - i
+            hat.set_pixel(x, y, *fucshia)
+            display(0.2)
+
+##
+### Challenge #3
+### Make a fire truck light
+##
+
+for n in range(5):
+    for x in range(width):
+        for y in range(height):
+            hat.set_pixel(x, y, *red)
+            hat.show()
+            time.sleep(0.1)
+        for y in range(height):
+            hat.set_pixel(x, y, *off)
+            hat.show()
+            time.sleep(0.1)
+
+##
+### Challenge #4
+### Make a police light
+##
+
+for n in range(5):
+    for x in range(width):
+        for y in range(height):
+            if x < width / 2:
+                hat.set_pixel(x, y, *red)
+            else:
+                hat.set_pixel(x, y, *blue)
+    display(0.2)
+    for x in range(width):
+        for y in range(height):
+            if x < width / 2:
+                hat.set_pixel(x, y, *blue)
+            else:
+                hat.set_pixel(x, y, *red)
+    display(0.2)
+
+##
+### Challenge #5
+### Light up only even columns (or rows) using only two FOR loops
+### and using a range operator in the outer loop
+##
+
+for x in range(0, width, 2):
+    for y in range(height):
+        hat.set_pixel(x, y, *green)
+display(5)
